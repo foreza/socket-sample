21a858a815c53579d34c6272ca3a738fb5ed7a05
Jason Chiu <jforeza@gmail.com>
2020-03-01 13:37:38 -0800

    initial socket.io project

---
+++ routes/index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+var server = require('../bin/www')
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: `Hello` });
+});
+
+
+
+
+
+
+module.exports = router;
